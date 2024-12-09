07a0602bfac51cc555bc9e3951e55c23b72ec251
Michal Struck <michal.o.struck@gmail.com>
2024-12-09 15:15:32 +0100

    bg settings

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ async function processBatch(files) {
 
     try {
       await sharp(filePath)
-        .trim()
+        .trim({ background: { r: 0, g: 0, b: 0, alpha: 0 }, threshold: 0 })
         .resize({
           width: fixedWidth,
           height: fixedHeight,
